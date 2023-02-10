e3f9a582c33cbe71b8386c2091866b982b4972de
Joao-Pedro-Fernandes <Joao-Pedro-Fernandes@users.noreply.github.com>
2023-02-10 18:14:57 -0300

    Adicionando telas ao programa
    Adicionando o botão de "Menu" ao programa. Adicionando mais telas ao programa.

--- app.py
+++ app.py
@@ -5,12 +5,19 @@ from kivy.lang import Builder
 from functions import *
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+
 Builder.load_file('app.kv')
 
 class AppWindow(Screen):
     pass
 
 class PesquisarWindow(Screen):
+    pass
+
+class MostrarWindow(Screen):
+    pass
+
+class FaltosasWindow(Screen):
     pass
 
 class Screens(ScreenManager):
